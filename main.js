e46180b793bf787ce4124d894376e9fabec45d17
Ferran Cipres <ferrancipres@gmail.com>
2023-07-21 21:17:08 +0200

    Last update 21/07 21.17

--- main.js
+++ main.js
@@ -11,24 +11,25 @@ let newElement = document.createElement('li');
 newElement.textContent = arrayResult;
 ul.appendChild(newElement);*/
 
-let btn = document.querySelector('.form__btn');
-let input = document.querySelector('.form__input');
-let userList = document.querySelector('#users_li');
+let btn = document.querySelector('.form__btn')
+let input = document.querySelector('.form__input')
+let userList = document.querySelector('#users_li')
 let homeContainer = document.querySelector('.form_container')
 let gameContainer = document.querySelector(".game_container")
 let spaceGuessLetter = document.querySelector(".space_guess_letter")
+let img = document.querySelector(".img_main_container")
 
 // Asginamos valor a cada tecla pulsada
-let keys = document.querySelectorAll('input[type="button"]');
+/*let keys = document.querySelectorAll('input[type="button"]');
 keys.forEach((key) => {
     key.addEventListener("click", (event) => {
         let selectedKey = event.target.value
         console.log(selectedKey)
     });
 })
+*/
 
-
-btn.addEventListener('click', addUserName);
+btn.addEventListener('click', addUserName)
 let myArray = []; /* Dentro de la array tienen que ir objetos */
 
 function addUserName() {
@@ -39,49 +40,56 @@ function addUserName() {
     };
 
     /*Meter objeto dentro de la array */
-    myArray += JSON.stringify(myObject);
+    myArray += JSON.stringify(myObject)
 
     /*Meter objeto dentro del localStorage*/
     localStorage.setItem("user-name", (myArray))
     console.log(myArray)
 
     /*Copia en users scores */
-    let newElement = document.createElement('li');
-    newElement.textContent = myObject.name;
-    userList.appendChild(newElement);
+    let newElement = document.createElement('li')
+    newElement.textContent = myObject.name
+    userList.appendChild(newElement)
 
     /* display form__container */
-    homeContainer.style.display = 'none';
-    gameContainer.style.display = "block";
+    homeContainer.style.display = 'none'
+    gameContainer.style.display = "block"
+
 
     // Definir string de 4 letras
     let fourLetterString = ["oído", "oyes", "oyen", "ovni", "ovil", "ovar", "oval", "otro"]
     console.log(fourLetterString)
 
     //Elegimos una palabra random y la dividimos en letras
-    let wordSelected = fourLetterString[Math.floor((Math.random() * fourLetterString.length))];
-    let wordsplited = wordSelected.split("");
+    let wordSelected = fourLetterString[Math.floor((Math.random() * fourLetterString.length))]
+    let wordsplited = wordSelected.split("")
 
     console.log(wordSelected)
     console.log(wordsplited)
 
-    // Convertimos cada letra en guión bajo y lo añadimos al espacio
     wordsplited.forEach((letter) => {
         letter = "_"
-        console.log(letter)
         spaceGuessLetter.textContent += letter
     });
+    
 
+    // Esto es una prueba
+    let keys = document.querySelectorAll('input[type="button"]')
 
+    keys.forEach((key) => {
+        key.addEventListener("click", (event) => {
+            let selectedKey = event.target.value
+            console.log(selectedKey)
+            
+            for(let i = 0; i < wordsplited.length; ++i) {
+                
+                if(wordsplited[i] == selectedKey) {
+                    alert('OK')
+                    
+                } else {
+                    alert('NOT OK')
+                }
+            }
+        });
+    })
 }
-
-/*switch (wordsplited) {
-    case 0:
-        spaceGuessLetter.textContent == key.value;
-        break;
-
-    case 1:
-        spaceGuessLetter.textContent !== key.value;
-        alert('error');
-}
-*/
