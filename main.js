cfa6e12616629859e0fe31ae7ac4364f71b1101b
Ferran Cipres <ferrancipres@gmail.com>
2023-07-21 09:07:30 +0200

    Update files

---
+++ main.js
@@ -0,0 +1,7 @@
+/*
+### Normativa ###
+. Cada uno responsable de su parte (no intromision)
+. ';' -> Poner siempre
+. Antes de finalizar una sección, presentar a equipo**
+. Añadir un comentario en cada sección / apartado
+*/
