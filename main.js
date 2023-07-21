8c530f9d4cdca26cc402788464f1401b6b8c5b63
Miguel Mayans <miguelmayansv@gmail.com>
2023-07-21 12:25:56 +0200

    Fase 1 completed

--- main.js
+++ main.js
@@ -15,20 +15,27 @@ let btn = document.querySelector('.form__btn');
 let input = document.querySelector('.form__input');
 let userList = document.querySelector('#users_li');
 
+
 btn.addEventListener('click', addUserName);
 let myArray = []; /* Dentro de la array tienen que ir objetos */
 
 function addUserName() {
+    event.preventDefault()
     /*Coge el valor del input*/
     let myObject = {
         name: input.value,
     };
 
     /*Meter objeto dentro de la array */
-    myArray += myObject;
- 
+    myArray += JSON.stringify(myObject);
+
+    /*Meter objeto dentro del localStorage*/
+    localStorage.setItem("user-name", (myArray))
+    console.log(myArray)
+
     /*Copia en users scores */
     let newElement = document.createElement('li');
     newElement.textContent = myObject.name;
     userList.appendChild(newElement);
+
 }
