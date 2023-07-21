c3765d3c00d0cd59ba5ace7827044afe2a4cba2b
Ferran Cipres <ferrancipres@gmail.com>
2023-07-21 11:44:26 +0200

    list_userName

--- main.js
+++ main.js
@@ -5,3 +5,30 @@
 . Antes de finalizar una sección, presentar a equipo**
 . Añadir un comentario en cada sección / apartado
 */
+
+/* Create new element 
+let newElement = document.createElement('li');
+newElement.textContent = arrayResult;
+ul.appendChild(newElement);*/
+
+let btn = document.querySelector('.form__btn');
+let input = document.querySelector('.form__input');
+let userList = document.querySelector('#users_li');
+
+btn.addEventListener('click', addUserName);
+let myArray = []; /* Dentro de la array tienen que ir objetos */
+
+function addUserName() {
+    /*Coge el valor del input*/
+    let myObject = {
+        name: input.value,
+    };
+
+    /*Meter objeto dentro de la array */
+    myArray += myObject;
+ 
+    /*Copia en users scores */
+    let newElement = document.createElement('li');
+    newElement.textContent = myObject.name;
+    userList.appendChild(newElement);
+}
