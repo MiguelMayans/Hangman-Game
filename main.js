e4907254cef9c0741a242f6861b6f874e660e2b3
Miguel Mayans <miguelmayansv@gmail.com>
2023-07-24 12:39:18 +0200

    last update: 12:39 24/07

--- main.js
+++ main.js
@@ -17,17 +17,13 @@ let userList = document.querySelector('#users_li')
 let homeContainer = document.querySelector('.form_container')
 let gameContainer = document.querySelector(".game_container")
 let spaceGuessLetter = document.querySelector(".space_guess_letter")
-let img = document.querySelector(".img_main_container")
+let winGame = document.querySelector(".win_game")
+let lostGame = document.querySelector(".lost_game")
+let userScore = document.querySelector("#aside_section")
 
-// Asginamos valor a cada tecla pulsada
-/*let keys = document.querySelectorAll('input[type="button"]');
-keys.forEach((key) => {
-    key.addEventListener("click", (event) => {
-        let selectedKey = event.target.value
-        console.log(selectedKey)
-    });
-})
-*/
+winGame.style.display = "none"
+lostGame.style.display = "none"
+
 
 btn.addEventListener('click', addUserName)
 let myArray = []; /* Dentro de la array tienen que ir objetos */
@@ -67,31 +63,89 @@ function addUserName() {
     console.log(wordSelected)
     console.log(wordsplited)
 
-    wordsplited.forEach((letter) => {
-        letter = "_"
-        spaceGuessLetter.textContent += letter
-    });
 
+    let wordFound = wordsplited.map(() => "_").join("");
+    spaceGuessLetter.textContent = wordFound;
+
+    // Seteamos contadores a 0
+    let wrongCounter = 0;
+    let rightCounter = 0;
+    let hangMan = 0;
 
     // Esto es una prueba
     let keys = document.querySelectorAll('input[type="button"]')
 
+    // Asignamos cada valor a cada tecla pulsada
     keys.forEach((key) => {
         key.addEventListener("click", (event) => {
             let selectedKey = event.target.value
+            let letterFound = false;
             console.log(selectedKey)
 
             for (let i = 0; i < wordsplited.length; ++i) {
 
-                if (wordsplited[i].includes(selectedKey)) {
-                    wordsplited[i] = selectedKey
+                if (wordsplited[i] === selectedKey) {
 
-
-                } else if (!wordsplited[i].includes(selectedKey)) {
-                    wordsplited[i] = "_"
+                    wordFound = wordFound.substring(0, i) + selectedKey + wordFound.substring(i + 1);
+                    letterFound = true
+                    rightCounter++
+                    console.log(rightCounter)
                 }
+
             }
-            spaceGuessLetter.textContent = wordsplited.join("")
+            if (!letterFound) {
+                spaceGuessLetter.textContent = wordFound;
+                wrongCounter++
+                hangMan++
+                console.log(wrongCounter)
+
+            } else {
+                spaceGuessLetter.textContent = wordFound;
+            }
+
+            if (wrongCounter === 6) {
+                gameContainer.style.display = "none"
+                lostGame.style.display = "block"
+                userScore.style.display = "none"
+            }
+
+            if (!wordFound.includes("_")) {
+                gameContainer.style.display = "none"
+                winGame.style.display = "block"
+                userScore.style.display = "none"
+            }
+
+            switch (hangMan) {
+                case 1:
+                    img.setAttribute("src", "/assets/img/hangman-step1.jpeg")
+                    break
+                case 2:
+                    img.setAttribute("src", "/assets/img/hangman-step2.jpeg")
+                    break
+                case 3:
+                    img.setAttribute("src", "/assets/img/hangman-step3.jpeg")
+                    break
+                case 4:
+                    img.setAttribute("src", "/assets/img/hangman-step4.jpeg")
+                    break
+                case 5:
+                    img.setAttribute("src", "/assets/img/hangman-step5.jpeg")
+                    break
+                case 6:
+                    img.setAttribute("src", "/assets/img/hangman-step6.jpeg")
+                    break
+            }
         });
     })
+
+    let img = document.querySelector(".img_main_container")
+
+
+
+
+
+
+
+
 }
+
